ad51566316bf69697533862301178d25641befe8
harsh-eagle-sigmoid <harshal.anil@sigmoidanalytics.com>
2026-01-06 15:27:00 +0530

    fix: remove proxies from ChatGroq

--- backend/app/langchain/chains.py
+++ backend/app/langchain/chains.py
@@ -3,9 +3,13 @@ from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
+for k in ["HTTP_PROXY", "HTTPS_PROXY", "http_proxy", "https_proxy"]:
+    os.environ.pop(k, None)
+
 llm = ChatGroq(
-    groq_api_key=os.environ["GROQ_API_KEY"],
-    model_name="llama3-70b-8192"
+    model="llama3-8b-8192",
+    api_key=os.environ["GROQ_API_KEY"],
+    temperature=0,
 )
 
 prompt = PromptTemplate(
